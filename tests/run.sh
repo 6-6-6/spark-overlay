398db2f31bd9b8823e0ca627608f5c40f39fc46a
Yuan Liao <liaoyuan@gmail.com>
2021-07-10 09:42:42 -0700

    tests/run.sh: Use new Docker image with more pre-installed packages
    Signed-off-by: Yuan Liao <liaoyuan@gmail.com>

--- tests/run.sh
+++ tests/run.sh
@@ -3,5 +3,5 @@
 ebuild-cmder \
     --portage-config tests/portage-config \
     --custom-repo . \
-    --docker-image ghcr.io/leo3418/gentoo-stage3-amd64-openjdk \
+    --docker-image ghcr.io/leo3418/gentoo-stage3-amd64-java \
     tests/test-cases/kotlin-latest.sh
