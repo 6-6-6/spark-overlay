151727ff42f99717c87d1bee06742c2b4c98a84e
Yuan Liao <liaoyuan@gmail.com>
2021-07-10 13:43:31 -0700

    Implement a new ebuild-like test script format
    Signed-off-by: Yuan Liao <liaoyuan@gmail.com>

--- tests/run.sh
+++ tests/run.sh
@@ -1,7 +1,32 @@
 #!/usr/bin/env bash
 
-ebuild-cmder \
-    --portage-config tests/portage-config \
-    --custom-repo . \
-    --docker-image ghcr.io/leo3418/gentoo-stage3-amd64-java \
-    tests/test-cases/kotlin-latest.sh
+main() {
+    for script in "$@"; do
+        . "${script}"
+
+        args=(
+            ebuild-cmder
+            --portage-config "${PORTAGE_CONFIG:-tests/portage-config/default}"
+            --docker-image "${DOCKER_IMAGE:-ghcr.io/leo3418/gentoo-stage3-amd64-java}"
+            ${PROFILE:+--profile ${PROFILE}}
+            ${GENTOO_REPO:+--gentoo-repo ${GENTOO_REPO}}
+            ${THREADS:+--threads ${THREADS}}
+            ${EMERGE_OPTS:+--emerge-opts ${EMERGE_OPTS}}
+            ${PULL:+--pull}
+            ${STORAGE_OPTS:+--storage-opts ${STORAGE_OPTS}}
+        )
+        args+=( --custom-repo . )
+        for repo in "${CUSTOM_REPOS[@]}"; do
+            args+=( --custom-repo "${repo}" )
+        done
+
+        # Pipe the run_test function's body into ebuild-commander
+        type run_test | sed '1,3d;$d' | "${args[@]}"
+    done
+}
+
+if [[ -n "$@" ]]; then
+    main "$@"
+else
+    main tests/test-cases/*
+fi
--- tests/test-cases/kotlin-latest.sh
+++ tests/test-cases/kotlin-latest.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-USE="binary" emerge -1 dev-java/kotlin-stdlib{,-js} dev-java/kotlin-reflect
-USE="javascript" emerge dev-lang/kotlin-bin
-emerge -1 dev-java/kotlin-stdlib{,-js} dev-java/kotlin-reflect
-emerge dev-java/kotlin-stdlib-jdk8
-FEATURES="test" emerge -1 dev-java/kotlin-stdlib{,-jdk7,-jdk8} dev-java/kotlin-test{,-junit,-annotations-common}
+run_test() {
+    USE="binary" emerge -1 dev-java/kotlin-stdlib{,-js} dev-java/kotlin-reflect
+    USE="javascript" emerge dev-lang/kotlin-bin
+    emerge -1 dev-java/kotlin-stdlib{,-js} dev-java/kotlin-reflect
+    emerge dev-java/kotlin-stdlib-jdk8
+    FEATURES="test" emerge -1 \
+        dev-java/kotlin-stdlib{,-jdk7,-jdk8} \
+        dev-java/kotlin-test{,-junit,-annotations-common}
+}
