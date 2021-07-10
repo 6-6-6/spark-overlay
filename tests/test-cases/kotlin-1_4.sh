f41a30e702e54ae52a9d62f2447675d1f3dbc03b
Yuan Liao <liaoyuan@gmail.com>
2021-07-10 14:34:34 -0700

    Add test case for Kotlin 1.4 packages

---
+++ tests/test-cases/kotlin-1_4.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+PORTAGE_CONFIG="tests/portage-config/kotlin-1_4"
+
+run_test() {
+    USE="binary" emerge -1 \
+        dev-java/kotlin-stdlib{,-js}:1.4 \
+        dev-java/kotlin-reflect:1.4
+    USE="javascript" emerge dev-lang/kotlin-bin
+    emerge -1 dev-java/kotlin-stdlib{,-js}:1.4 dev-java/kotlin-reflect:1.4
+    emerge dev-java/kotlin-stdlib-jdk8:1.4
+    FEATURES="test" emerge -1 \
+        dev-java/kotlin-stdlib{,-jdk7,-jdk8}:1.4 \
+        dev-java/kotlin-test{,-junit,-annotations-common}:1.4
+}
