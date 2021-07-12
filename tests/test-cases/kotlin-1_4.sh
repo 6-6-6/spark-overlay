f1b5e70db8e0bf62729469f02e750b08456ef6c6
Yuan Liao <liaoyuan@gmail.com>
2021-07-12 14:22:28 -0700

    Update test case for Kotlin 1.4 packages
    Signed-off-by: Yuan Liao <liaoyuan@gmail.com>

--- tests/test-cases/kotlin-1_4.sh
+++ tests/test-cases/kotlin-1_4.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-PORTAGE_CONFIG="tests/portage-config/kotlin-1_4"
-
 run_test() {
     USE="binary" emerge -1 \
         dev-java/kotlin-stdlib{,-js}:1.4 \
         dev-java/kotlin-reflect:1.4
-    USE="javascript" emerge dev-lang/kotlin-bin
+    USE="javascript" emerge dev-lang/kotlin-bin:1.4
     emerge -1 dev-java/kotlin-stdlib{,-js}:1.4 dev-java/kotlin-reflect:1.4
     emerge dev-java/kotlin-stdlib-jdk8:1.4
     FEATURES="test" emerge -1 \
