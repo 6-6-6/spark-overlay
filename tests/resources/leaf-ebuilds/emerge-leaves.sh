d0d6912eb44cb09f857ff3b02f07afa70d99bac6
Yuan Liao <liaoyuan@gmail.com>
2021-07-20 21:17:58 -0700

    Skip test for masked java-virtuals/antlr:4
    Signed-off-by: Yuan Liao <liaoyuan@gmail.com>

--- tests/resources/leaf-ebuilds/emerge-leaves.sh
+++ tests/resources/leaf-ebuilds/emerge-leaves.sh
@@ -6,6 +6,8 @@ EXCLUDED_EBUILDS=(
     "dev-java/kotlin-stdlib-jdk8-1.5.20"
     "dev-java/kotlin-test-js-1.4.32"
     "dev-java/kotlin-test-js-1.5.20"
+    # Masked packages
+    "java-virtuals/antlr-4.8"
 )
 
 : ${ELEAVES:=/var/db/repos/spark-overlay/tests/resources/leaf-ebuilds/eleaves.py}
