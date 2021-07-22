9032d4342b96e56226099c6275b91d26493aa370
Yuan Liao <liaoyuan@gmail.com>
2021-07-21 23:24:22 -0700

    emerge-leaves.sh: Specify excluded ebuilds with regular expressions
    Signed-off-by: Yuan Liao <liaoyuan@gmail.com>

--- tests/resources/leaf-ebuilds/emerge-leaves.sh
+++ tests/resources/leaf-ebuilds/emerge-leaves.sh
@@ -1,27 +1,29 @@
 #!/usr/bin/env bash
 
-EXCLUDED_EBUILDS=(
+EXCLUDED_EBUILD_PATTERNS=(
     # Kotlin ebuilds, covered by other test cases
-    "dev-java/kotlin-stdlib-jdk8-1.4.32"
-    "dev-java/kotlin-stdlib-jdk8-1.5.20"
-    "dev-java/kotlin-test-js-1.4.32"
-    "dev-java/kotlin-test-js-1.5.20"
-    # Masked packages
-    "java-virtuals/antlr-4.8"
+    "dev-java/kotlin-stdlib-jdk8-.*"
+    "dev-java/kotlin-test-js-.*"
 )
 
 : ${ELEAVES:=/var/db/repos/spark-overlay/tests/resources/leaf-ebuilds/eleaves.py}
 
-LEAF_EBUILDS=$("${ELEAVES}" --algorithm rev-search spark-overlay || exit $?)
-for excluded in "${EXCLUDED_EBUILDS[@]}"; do
-    LEAF_EBUILDS=${LEAF_EBUILDS//"${excluded}"}
-done
+LEAF_EBUILDS_RAW=$("${ELEAVES}" --algorithm rev-search spark-overlay || exit $?)
 echo -e "\nLeaf ebuilds to be tested:"
-sed -r '/^\s*$/d' <<< "$LEAF_EBUILDS"
+LEAF_EBUILDS=()
+for ebuild in ${LEAF_EBUILDS_RAW}; do
+    for pattern in "${EXCLUDED_EBUILD_PATTERNS[@]}"; do
+        if grep -q -x "${pattern}" <<< "${ebuild}"; then
+            # Match an ignore pattern, skip to the next ebuild
+            continue 2
+        fi
+    done
+    echo "${ebuild}"
+    LEAF_EBUILDS+=( "${ebuild}" )
+done
 
 FAILED_EBUILDS=()
-mkdir -p /etc/portage/package.use
-for ebuild in ${LEAF_EBUILDS}; do
+for ebuild in "${LEAF_EBUILDS[@]}"; do
     echo -e "\nEmerging ${ebuild}"
     emerge "=${ebuild}" || FAILED_EBUILDS+=( "${ebuild}" )
 done
