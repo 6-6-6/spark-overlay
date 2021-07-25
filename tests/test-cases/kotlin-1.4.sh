6fba2c0a42569f955c661b66dd43141f06215e8e
Yuan Liao <liaoyuan@gmail.com>
2021-07-25 12:54:35 -0700

    Update Kotlin ebuild test cases to improve coverage
    Signed-off-by: Yuan Liao <liaoyuan@gmail.com>

--- tests/test-cases/kotlin-1.4.sh
+++ tests/test-cases/kotlin-1.4.sh
@@ -3,11 +3,18 @@
 PORTAGE_CONFIGS=( tests/portage-configs/kotlin )
 
 run_test() {
+    # Library bootstrap stage 1
     USE="binary" emerge -1 \
         dev-java/kotlin-stdlib{,-js}:1.4 \
         dev-java/kotlin-reflect:1.4
     emerge dev-lang/kotlin-bin:1.4
+    # Library bootstrap stage 2
     emerge -1 dev-java/kotlin-stdlib{,-js}:1.4 dev-java/kotlin-reflect:1.4
+    # Library bootstrap stage 3
+    find /var/db/repos/spark-overlay -type d -name 'kotlin-core-*' -fprintf core-pkgs '%P:1.4\n'
+    xargs -a core-pkgs emerge -1 dev-java/kotlin-stdlib{,-js}:1.4 dev-java/kotlin-reflect:1.4
+    # Test additional packages
+    emerge -1 dev-java/kotlin-test-js:1.4
     emerge dev-java/kotlin-stdlib-jdk8:1.4
     FEATURES="test" emerge -1 \
         dev-java/kotlin-stdlib{,-jdk7,-jdk8}:1.4 \
