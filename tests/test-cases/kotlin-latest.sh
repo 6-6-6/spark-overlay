c38b0464013cbfb1c116f805c7f3ec4c435a9af7
Yuan Liao <liaoyuan@gmail.com>
2021-07-27 17:02:34 -0700

    Update test cases for changes in kotlin-stdlib-js installation steps
    Signed-off-by: Yuan Liao <liaoyuan@gmail.com>

--- tests/test-cases/kotlin-latest.sh
+++ tests/test-cases/kotlin-latest.sh
@@ -4,13 +4,14 @@ PORTAGE_CONFIGS=( tests/portage-configs/kotlin )
 
 run_test() {
     # Library bootstrap stage 1
-    USE="binary" emerge -1 dev-java/kotlin-stdlib{,-js} dev-java/kotlin-reflect
+    USE="binary" emerge -1 dev-java/kotlin-stdlib dev-java/kotlin-reflect
     emerge dev-lang/kotlin-bin
     # Library bootstrap stage 2
-    emerge -1 dev-java/kotlin-stdlib{,-js} dev-java/kotlin-reflect
+    emerge -1 dev-java/kotlin-stdlib dev-java/kotlin-reflect
     # Library bootstrap stage 3
-    find /var/db/repos/spark-overlay -type d -name 'kotlin-core-*' -fprintf core-pkgs '%P\n'
-    xargs -a core-pkgs emerge -1 dev-java/kotlin-stdlib{,-js} dev-java/kotlin-reflect
+    find /var/db/repos/spark-overlay -type d -name 'kotlin-core-*' \
+        -printf '%P\0' | xargs -0 emerge -1 \
+        dev-java/kotlin-stdlib{,-js} dev-java/kotlin-reflect
     # Test additional packages
     emerge -1 dev-java/kotlin-test-js
     emerge dev-java/kotlin-stdlib-jdk8
