25d568b24e5837a5e23c79f9e7b9014b8cfdbebb
Yuan Liao <liaoyuan@gmail.com>
2022-02-27 15:10:27 -0800

    Skip yecht bootstrap for leaf ebuild test cases as it's no longer needed
    Signed-off-by: Yuan Liao <liaoyuan@gmail.com>

--- tests/resources/leaf-ebuilds/test-case-tmpl.sh
+++ tests/resources/leaf-ebuilds/test-case-tmpl.sh
@@ -16,14 +16,5 @@ run_test() {
     # Install Kotlin 1.4
     emerge -1 dev-lang/kotlin-bin:1.4
 
-    # Prepare to bootstrap some packages
-    mkdir -p /etc/portage/package.use
-    echo "dev-java/yecht binary" \
-        >> /etc/portage/package.use/leaf-ebuilds-bootstrap
-    emerge -1 dev-java/yecht
-    # Bootstrap to stage 2 if USE="-binary"
-    rm /etc/portage/package.use/leaf-ebuilds-bootstrap
-    emerge -N1 dev-java/yecht
-
     /var/db/repos/spark-overlay/tests/resources/leaf-ebuilds/emerge-leaves.sh
 }
