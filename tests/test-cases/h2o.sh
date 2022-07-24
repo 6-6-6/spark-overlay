4142291a1168e99e3c0132a6d9e8e30c8fe6b897
Yuan Liao <liaoyuan@gmail.com>
2022-07-24 07:14:28 -0700

    Add latency for H2O demo script to reduce test failure due to bad timing
    Signed-off-by: Yuan Liao <liaoyuan@gmail.com>

--- tests/test-cases/h2o.sh
+++ tests/test-cases/h2o.sh
@@ -6,5 +6,5 @@ run_test() {
     emerge dev-python/h2o-py dev-java/h2o
     # Start an H2O server
     nohup "$(find /usr/bin -regex '/usr/bin/h2o-[0-9]+\.[0-9]+')" > /dev/null 2>&1 &
-    /var/db/repos/spark-overlay/tests/resources/h2o/run-builtin-demos.py
+    sleep 5 && /var/db/repos/spark-overlay/tests/resources/h2o/run-builtin-demos.py
 }
